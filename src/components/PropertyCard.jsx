bdc4f3c797bfc9618910c2e75414613f766313ea
Miguel Cartagena <miguecartagena@gmail.com>
2023-06-07 17:38:43 -0400

    mejoras en cards

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -2,12 +2,13 @@ import React, { useState, useContext } from "react";
 import FavButton from "./FavButtom";
 import { PropertyContext } from "../context/PropertyContext";
 import { FavoritesContext } from "../context/FavoritesContext";
-import "../css/propertyCard.css";
+import "../assets/whatsapp.png";
+import WspLogo from "../css/propertyCard.css";
 
 const PropertyCard = ({ property }) => {
 
   const { addFavorites, deleteFavorites, favorites } = useContext(FavoritesContext);
-  const { data} = useContext(PropertyContext);
+  const { data } = useContext(PropertyContext);
 
 
   const isPropertyInFavorites = favorites.some((item) => item.id === property.id);
@@ -25,10 +26,10 @@ const PropertyCard = ({ property }) => {
       <div className="card_content_property">
         <h1 className="card_title">{property.tipo}</h1>
         <div className="container_desc">
-      <img className="img" src={property.img} alt="" />
-        <h2 className="card_region">{property.region}</h2>
-        <p className="card_description"> {property.descripcion}</p>
-        <p className="card_price">{property.precio}</p>
+          <img className="img" src={property.img} alt="" />
+          <h2 className="card_region">{property.region}</h2>
+          <p className="card_description"> {property.descripcion}</p>
+          <p className="card_price">{property.precio}</p>
         </div>
       </div>
       <div className="button-fav">
@@ -40,10 +41,13 @@ const PropertyCard = ({ property }) => {
           <span className="circle" aria-hidden="true">
             <span className="icon arrow"></span>
           </span>
-          <span 
-          onClick=""
-          className="button-text">Agendar Visita</span>
+          <span
+            onClick=""
+            className="button-text">Agendar Visita</span>
         </button>
+        <a href="https://api.whatsapp.com/send?phone=56982248448" target="_blank" rel="noopener noreferrer">
+          <div class="wsp_icon"></div>
+        </a>
       </div>
     </article>
   );
